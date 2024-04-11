be1b32137810f6ac04a68980a5d619a50f3cd29a
Tomek <toxdg@o2.pl>
2024-04-11 13:52:54 +0200

    qrcode

--- ac_vcard.php
+++ ac_vcard.php
@@ -1,5 +1,4 @@
 <?php 
-
 /**
 * Plugin Name: AC vCard - Generator
 * Plugin URI: https://tox.ovh
@@ -9,6 +8,24 @@
 * Author URI: https://tox.ovh
 **/
 
+function ac_vcard_config(){
+	$qrCodeApiQRserver = 'https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=';
+	$qrCodeApiGoogle = 'https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=';
+	$qrCodeApi = $qrCodeApiQRserver;
+	$apiUrl = get_home_url().'/api_vcard/';
+	$rootDir = plugin_dir_path(__FILE__).'cards/';
+	$rootURL = plugin_dir_url(__FILE__).'cards/';
+
+	$returnArray = array(
+		"qrCodeApi" => $qrCodeApi,
+		"apiUrl" => $apiUrl,
+		"rootDir" => $rootDir, 
+		"rootUrl" => $rootURL,
+	);
+	return $returnArray;
+}
+
+
 add_action( 'template_redirect', 'redirect_post_type_single' );
 function redirect_post_type_single(){
     if ( ! is_singular( 'ac_vcard_cpt' ) )
@@ -16,6 +33,7 @@ function redirect_post_type_single(){
     wp_redirect( get_home_url(), 301 );
     exit;
 }
+
 // Register post type
 add_action('init', 'ac_vcard_cpt');
 function ac_vcard_cpt() {
@@ -42,6 +60,95 @@ function ac_vcard_cpt() {
     register_post_type( 'ac_vcard_cpt' , $args );
 }
 
+// columns list
+add_filter('manage_ac_vcard_cpt_posts_columns', 'ac_vcard_cpt_posts_columns');
+
+function ac_vcard_cpt_posts_columns($columns) {
+    $columnsMod = array();
+    $index = 0;
+    foreach ($columns as $key => $singleColum) {
+        $columnsMod[$key] = $singleColum;
+		if ($index == 0) {
+			// before post title 
+		}
+        if ($index == 1) {
+			// after post title 
+			$columnsMod['ac_post_id'] = "Post Data";
+			$columnsMod['photo'] = "Photo";
+            $columnsMod['qrcode'] = "API url qrcode";
+			$columnsMod['qrcode_src'] = "API url src";
+			$columnsMod['image_full'] = "Full qrcode";
+			$columnsMod['image_full_src'] = "Full qrcode src";
+			$columnsMod['image'] = "Image embed";	
+        }
+		if ($index == 2) {
+			//after post date
+		}
+        $index++;
+    }
+    return $columnsMod;
+}
+
+add_action('manage_ac_vcard_cpt_posts_custom_column', 'custom_ac_vcard_cpt_column', 10, 2);
+
+function custom_ac_vcard_cpt_column($column, $post_id) {
+    $meta = get_post_meta($post_id);
+    switch ($column) {
+		case 'ac_post_id':
+			$postData = get_post($post_id);
+            echo '<b>id:</b> '.$post_id;
+			echo '<br><b>slug:</b> '.$postData->post_name;
+			echo '<br><b>api url:</b> '.ac_vcard_config()['apiUrl'].$postData->post_name;
+            break;
+        case 'qrcode':
+			$postData = get_post($post_id);
+			$apiUrl = ac_vcard_config()['apiUrl'].$postData->post_name;
+			//echo '<img width="100px" height="100px" src="'..'">';
+            echo '<img width="180px" height="180px" src="'.ac_vcard_config()['qrCodeApi'].$apiUrl.'">';
+			//echo '<br>'.ac_vcard_config()['qrCodeApi'].$apiUrl;
+            break;
+		case 'qrcode_src':
+			$postData = get_post($post_id);
+			$apiUrl = ac_vcard_config()['apiUrl'].$postData->post_name;
+			echo '<br>'.ac_vcard_config()['qrCodeApi'].$apiUrl;
+            break;
+		case 'photo':
+			$cpt_PostMeta = get_post_meta($post_id);
+			if($cpt_PostMeta['ac_vcard__user_photo'][0] != ''){
+				echo "<img width='180px' height='180px' src='".wp_get_attachment_url($cpt_PostMeta['ac_vcard__user_photo'][0])."'>";
+			}else{
+				echo "no image";
+			}
+            break;
+		case 'image_full':
+			$cpt_PostMeta = get_post_meta($post_id);
+			$fileName = ac_vcard__GetFileNameCore($post_id).'.png';	
+			if (file_exists(ac_vcard_config()["rootDir"].$fileName)) {
+				echo '<img width="180px" height="180px" src="'.ac_vcard_config()["rootUrl"].$fileName.'">';
+				//echo '<br>'.ac_vcard_config()["rootUrl"].$fileName;
+			}else{
+				echo "no image";
+			}
+            break;
+		case 'image_full_src':
+			$cpt_PostMeta = get_post_meta($post_id);
+			$fileName = ac_vcard__GetFileNameCore($post_id).'.png';	
+			if (file_exists(ac_vcard_config()["rootDir"].$fileName)) {
+				echo '<br>'.ac_vcard_config()["rootUrl"].$fileName;
+			}else{
+				echo "no image";
+			}
+            break;
+		case 'image':
+			$postData = get_post($post_id);
+			$fileName = ac_vcard__GetFileNameCore($post_id).'.png';
+			echo 'Api URL: <br><textarea><img width="100px" height="100px" src="'.ac_vcard_config()['qrCodeApi'].$postData->post_name.'"></textarea>';
+			echo '<br><br>Full Data: <br><textarea><img style="max-width:200px; height:auto;" src="'.ac_vcard_config()['rootUrl'].$fileName.'"></textarea>';
+            break;
+    }
+}
+
+
 function ac_vcard_cpt_enqueue(){
 	$screen = get_current_screen();
 	$screen = $screen->post_type;
@@ -54,7 +161,10 @@ function ac_vcard_cpt_enqueue(){
 }
 add_action('admin_enqueue_scripts', 'ac_vcard_cpt_enqueue');
 
+
+
 include 'meta_box.php';
+require_once __DIR__ . '/vendor/phpqrcode/qrlib.php';
 require_once __DIR__ . '/vendor/autoload.php';
 use JeroenDesloovere\VCard\VCard;
 
@@ -84,7 +194,7 @@ add_action( 'parse_request', 'ac_vcard__api_main_function' );
 function ac_vcard__api_main_function( &$wp ){
     
     // template directory
-    $template_dir = ac_bup__config()['plugin_path'].'templates/';
+    //$template_dir = ac_bup__config()['plugin_path'].'templates/';
     // form url 
     $form_link_serv = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
     $form_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://".$form_link_serv;
@@ -100,80 +210,17 @@ function ac_vcard__api_main_function( &$wp ){
 		$nameUserCard = ac_vcard__urlParseToTemplate();
 		$nameUserCard = str_replace('/api_vcard/', "", $nameUserCard['core_name']);
 		$nameUserCard = explode("/", $nameUserCard)[0];
-
-		$cpt_PostMeta = get_post_meta(ac_vcard_get_postid_by_slug($nameUserCard));
-		//var_dump($cpt_PostMeta);
 		
-		// generate vcard
-		$vcard = new VCard();
-		// define variables
-		$lastname = $cpt_PostMeta['ac_vcard__user_fname'][0];
-		$firstname = $cpt_PostMeta['ac_vcard__user_lname'][0];
-		$additional = '';
-		$prefix = '';
-		$suffix = '';
-
-		// add personal data
-		$vcard->addName($lastname, $firstname, $additional, $prefix, $suffix);
-
-		// add work data
-		if($cpt_PostMeta['ac_vcard__user_cpompanyname'][0] != ''){
-			$vcard->addCompany($cpt_PostMeta['ac_vcard__user_cpompanyname'][0]);
-		}
+		$cptUserPostID = ac_vcard_get_postid_by_slug($nameUserCard);
+		$cpt_PostMeta = get_post_meta($cptUserPostID);
 		
-		if($cpt_PostMeta['ac_vcard__user_titleposition'][0] != ''){
-			$vcard->addJobtitle($cpt_PostMeta['ac_vcard__user_titleposition'][0]);
-		}
+		ac_vcard__generateVCF($cptUserPostID);
 		
-		if($cpt_PostMeta['ac_vcard__user_emailadress'][0] != ''){
-			$vcard->addEmail($cpt_PostMeta['ac_vcard__user_emailadress'][0]);
-		}
+		$fileName = ac_vcard__GetFileNameCore($cptUserPostID);
 		
-		if($cpt_PostMeta['ac_vcard__user_emailadress'][0] != ''){
-			$vcard->addEmail($cpt_PostMeta['ac_vcard__user_emailadress'][0]);
-		}
+		$fileVCF = $fileName.'.vcf';
 		
-		if($cpt_PostMeta['ac_vcard__user_webside'][0] != ''){
-			$vcard->addURL($cpt_PostMeta['ac_vcard__user_webside'][0]);
-		}
-		
-		if($cpt_PostMeta['ac_vcard__user_mobilenumber'][0] != ''){
-			$vcard->addPhoneNumber($cpt_PostMeta['ac_vcard__user_mobilenumber'][0], 'PREF;WORK');
-		}
-		
-		if($cpt_PostMeta['ac_vcard__user_phonenumber'][0] != ''){
-			$vcard->addPhoneNumber($cpt_PostMeta['ac_vcard__user_phonenumber'][0], 'WORK');
-		}
-		if($cpt_PostMeta['ac_vcard__user_fulladress'][0] != ''){
-			$vcard->addAddress(null, null, $cpt_PostMeta['ac_vcard__user_fulladress'][0], null, null, null, null);
-		}
-		if($cpt_PostMeta['ac_vcard__user_photo'][0] != ''){
-			$vcard->addPhoto(wp_get_original_image_path($cpt_PostMeta['ac_vcard__user_photo'][0]));
-		}
-		
-		//$vcard->addRole('Data Protection Officer');
-		//$vcard->addEmail('info@jeroendesloovere.be');
-		//$vcard->addPhoneNumber(1234121212, 'PREF;WORK');
-		//$vcard->addPhoneNumber(123456789, 'WORK');
-		//$vcard->addAddress(null, null, 'street', 'worktown', null, 'workpostcode', 'Belgium');
-		//$vcard->addLabel('street, worktown, workpostcode Belgium');
-		//$vcard->addURL('http://www.jeroendesloovere.be');
-
-		//$vcard->addPhoto(__DIR__ . '/landscape.jpeg');
-
-		// return vcard as a string
-		//return $vcard->getOutput();
-
-		// return vcard as a download
-		//return $vcard->download();
-		
-		$rootDir = plugin_dir_path(__FILE__).'cards/';
-		if (!file_exists($rootDir)) {
-			mkdir($rootDir, 0777, true);
-		}
-		$file = $firstname.'-'.$lastname.'.vcf';
-		file_put_contents($rootDir.$file, $vcard->getOutput());
-		header("Location: ".plugin_dir_url(__FILE__).'cards/'.$file);
+		header("Location: ".plugin_dir_url(__FILE__).'cards/'.$fileVCF);
 		exit();
     }           
     return;
@@ -182,6 +229,135 @@ function ac_vcard__api_main_function( &$wp ){
 /* ROUTER END */
 
 /* ROUTER FUNCTION */
+// sanitize_filename
+function ac_vcard__sanitizeFilename($fileName){
+	$fileName = str_replace(' ', '', $fileName);
+	$fileName = ac_vcard__sanitizeString($fileName);
+	return $fileName;
+}
+
+function ac_vcard__sanitizeString($string){
+	$input = array('ą', 'Ą', 'ć', 'Ć', 'ę', 'Ę', 'ł', 'Ł', 'ń', 'Ń', 'ó', 'Ó', 'ś', 'Ś', 'ź', 'Ź', 'ż', 'Ż');
+	$output = array('a', 'A', 'c', 'C', 'e', 'E', 'l', 'L', 'n', 'N', 'o', 'O', 's', 'S', 'z', 'Z', 'z', 'Z');
+	$stringReturn = str_replace($input, $output, $string);
+	return $stringReturn;
+}
+
+// get core filename
+function ac_vcard__GetFileNameCore($acPostId){
+	$cpt_PostMeta = get_post_meta($acPostId);
+	$lastname = $cpt_PostMeta['ac_vcard__user_fname'][0];
+	$firstname = $cpt_PostMeta['ac_vcard__user_lname'][0];
+	$fileName = strtolower($firstname.'-'.$lastname);
+	$fileName = ac_vcard__sanitizeFilename($fileName);
+	return $fileName;
+}
+
+// generate VCF
+function ac_vcard__generateVCF($cptUserPostID){
+	
+	$rootDir = plugin_dir_path(__FILE__).'cards/';
+	$rootDirIndex = $rootDir.'index.php';
+	if (!file_exists($rootDir)) {
+		mkdir($rootDir, 0777, true);
+
+	}
+	if (!file_exists($rootDirIndex)) {
+		file_put_contents($rootDirIndex, '<?php // silence ?>');
+	}
+	
+	$cpt_PostMeta = get_post_meta($cptUserPostID);
+	// generate vcard
+	$vcard = new VCard();
+	// define variables
+	$lastname = $cpt_PostMeta['ac_vcard__user_fname'][0];
+	$firstname = $cpt_PostMeta['ac_vcard__user_lname'][0];
+	$additional = '';
+	$prefix = '';
+	$suffix = '';
+
+	$tmpName = $lastname.' '.$firstname;
+	// add personal data
+	$vcard->addName($lastname, $firstname, $additional, $prefix, $suffix);
+	//$vcard->addName($tmpName, '('.$cpt_PostMeta['ac_vcard__user_cpompanyname'][0].')', $additional, $prefix, $suffix);
+	
+	// add work data
+	if($cpt_PostMeta['ac_vcard__user_cpompanyname'][0] != ''){
+		$vcard->addCompany($cpt_PostMeta['ac_vcard__user_cpompanyname'][0]);
+	}
+	
+	if($cpt_PostMeta['ac_vcard__user_titleposition'][0] != ''){
+		$vcard->addRole($cpt_PostMeta['ac_vcard__user_titleposition'][0]);
+	}
+	
+	if($cpt_PostMeta['ac_vcard__user_emailadress'][0] != ''){
+		$vcard->addEmail($cpt_PostMeta['ac_vcard__user_emailadress'][0]);
+	}
+
+	if($cpt_PostMeta['ac_vcard__user_webside'][0] != ''){
+		$vcard->addURL($cpt_PostMeta['ac_vcard__user_webside'][0]);
+	}
+	
+	if($cpt_PostMeta['ac_vcard__user_mobilenumber'][0] != ''){
+		$vcard->addPhoneNumber($cpt_PostMeta['ac_vcard__user_mobilenumber'][0], 'PREF;WORK');
+	}
+	
+	if($cpt_PostMeta['ac_vcard__user_phonenumber'][0] != ''){
+		$vcard->addPhoneNumber($cpt_PostMeta['ac_vcard__user_phonenumber'][0], 'WORK');
+	}
+	if($cpt_PostMeta['ac_vcard__user_fulladress'][0] != ''){
+		$addressPart = $cpt_PostMeta['ac_vcard__user_fulladress'][0];
+		$addressPart = ac_vcard__sanitizeString($addressPart);
+		$addressPart = explode(",", $addressPart);
+		$vcard->addAddress(null, null, $addressPart[0], $addressPart[2], null, $addressPart[1], $addressPart[3]);
+		
+		//$vcard->addAddress(null, null, $cpt_PostMeta['ac_vcard__user_fulladress'][0], null, null, null, null);
+	}
+	
+	if($cpt_PostMeta['ac_vcard__user_photo'][0] != ''){
+		//$vcard->addPhoto(wp_get_original_image_path($cpt_PostMeta['ac_vcard__user_photo'][0]));
+	}
+	
+	//$vcard->addJobtitle($cpt_PostMeta['ac_vcard__user_titleposition'][0]);
+	//$vcard->addRole('Data Protection Officer');
+	//$vcard->addEmail('info@jeroendesloovere.be');
+	//$vcard->addPhoneNumber(1234121212, 'PREF;WORK');
+	//$vcard->addPhoneNumber(123456789, 'WORK');
+	//$vcard->addAddress(null, null, 'street', 'worktown', null, 'workpostcode', 'Belgium');
+	//$vcard->addLabel('street, worktown, workpostcode Belgium');
+	//$vcard->addURL('http://www.jeroendesloovere.be');
+
+	//$vcard->addPhoto(__DIR__ . '/landscape.jpeg');
+
+	// return vcard as a string
+	//return $vcard->getOutput();
+
+	// return vcard as a download
+	//return $vcard->download();
+	
+	
+	$fileName = ac_vcard__GetFileNameCore($cptUserPostID);
+	$fileVCF = $fileName.'.vcf';
+	
+	if (file_exists($rootDir.$fileVCF)) {
+		if(date ("YmdHis", filemtime($rootDir.$fileVCF)) <=  $cpt_PostMeta['ac_vcard__date_update'][0]){
+			file_put_contents($rootDir.$fileVCF, $vcard->getOutput());
+		}
+	}else{
+		file_put_contents($rootDir.$fileVCF, $vcard->getOutput());
+	}
+	
+	$imageFile = $fileName.'.png';
+	// https://codedamn.com/news/web-development/php-qr-code-generator-with-examples
+	QRcode::png($vcard->getOutput(), $rootDir.$imageFile);
+	
+	
+	$postData = get_post($cptUserPostID);
+	$apiUrl = ac_vcard_config()['apiUrl'].$postData->post_name;
+	$imageFile = $fileName.'-url.png';
+	QRcode::png($apiUrl, $rootDir.$imageFile);
+}
+
 // Parse url to template file name
 function ac_vcard__urlParseToTemplate(){
 	$site = str_replace("http://", "", get_site_url());
@@ -223,6 +399,7 @@ function ac_vcard__page_404(){
     get_template_part( 404 );
     exit();
 }
+
 // validate url
 function ac_vcard__url_return_error($url){
 	$url_array = explode("/", $url);
@@ -240,3 +417,51 @@ function ac_vcard__url_return_error($url){
 	}
 	return false;
 }
+
+/* CRON FUNCTION */
+// init cron function
+add_action( 'init', 'ac_vcard__removeOldFiles', 0 );
+function ac_vcard__removeOldFiles(){
+	if (isset($_GET["ac_vcard_cron"]) && $_GET["ac_vcard_cron"] === 'true') {
+        ac_vcard__removeOldFilesCleaner();
+    }
+    $cron_day = intval(get_option('ac_vcard_cron_day'));
+    $currentDate = date("Ymd");
+    if($currentDate > $cron_day){
+        ac_vcard__removeOldFilesCleaner();
+        update_option( 'ac_vcard_cron_day', $currentDate );
+    }
+}
+
+// main cleaner
+function ac_vcard__removeOldFilesCleaner(){
+	$args = array(
+        'post_type' => 'ac_vcard_cpt',
+        'posts_per_page' => -1,
+    );
+	$fileArray = array();
+	$fileArray[] = 'index.php';
+    $ac_vcard_cpt_list = new WP_Query( $args );
+	if ($ac_vcard_cpt_list->have_posts()):
+        while ( $ac_vcard_cpt_list->have_posts() ) : $ac_vcard_cpt_list->the_post();
+			$acPostId = get_the_ID();
+			
+			$fileName = ac_vcard__GetFileNameCore($acPostId);
+			$fileArray[] = $fileName.'.vcf';
+			$fileArray[] = $fileName.'.png';
+			$fileArray[] = $fileName.'-url.png';
+			
+		endwhile;                
+    endif;
+	wp_reset_query();
+	$filesDir = scandir(ac_vcard_config()['rootDir']);
+	$filesDir = array_diff($filesDir, array('.', '..'));
+	
+	foreach($filesDir as $key => $TMPfile) {
+		if(!in_array($TMPfile, $fileArray)) {
+			unlink(ac_vcard_config()['rootDir'].$TMPfile);
+		}
+	}
+}
+
+
--- meta_box.php
+++ meta_box.php
@@ -14,7 +14,7 @@ function ac_vcard_register_meta_boxes() {
 		'ac_vcard_cpt',
 		'advanced', 
 		'high' // priority
-		);
+	);
 }
 add_action( 'add_meta_boxes', 'ac_vcard_register_meta_boxes' );
 
@@ -22,13 +22,14 @@ function ac_vcard_cpt_display_callback( $post ) {
 	// Display code/markup goes here. Don't forget to include nonces!'
 	$ac_postID = $post->ID;
 	$cpt_PostMeta = get_post_meta($ac_postID);
-	//var_dump($cpt_PostMeta);
 	
-	$apiUrl = get_home_url().'/api_vcard/'.$post->post_name;
+	$apiUrl = ac_vcard_config()['apiUrl'].$post->post_name;
+
+	$qrCodeApi = ac_vcard_config()['qrCodeApi'];
 	?>
 	<input type="hidden" name="ac_vcard" value="ac_vcard_save" />
 	<input type="hidden" name="ac_vcard__title" id="ac_vcard__title" value="">
-	
+	<input type="hidden" name="ac_vcard__date_update" id="ac_vcard__date_update" value="<?php echo date("YmdHis")?>">
 	<style>
 		.ac-vcard-form input,.ac-vcard-form textarea{
 			width:100%;
@@ -39,7 +40,6 @@ function ac_vcard_cpt_display_callback( $post ) {
 			height:auto;
 		}
 	</style>
-	<?php?>
 	<table class="form-table ac-vcard-form">
 		<tr>
 			<th><label for="ac_vcard__title_show"><?php _e("Api url", 'ac_vcard'); ?></label></th>
@@ -47,10 +47,29 @@ function ac_vcard_cpt_display_callback( $post ) {
 			</td>            
         </tr>
 		<tr>
-			<th><label for="ac_vcard__qrcode_embed"><?php _e("Embed QR code", 'ac_vcard'); ?></label></th>
+			<th><label for="ac_vcard__qrcode_embed"><?php _e("API QR code", 'ac_vcard'); ?></label></th>
 			<td>
-			<img src="https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=<?php echo $apiUrl; ?>"><br>
-			<textarea name="ac_vcard__qrcode_embed" ><img src="https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=<?php echo $apiUrl; ?>"></textarea>
+			<img width='180px' height='180px' src="<?php echo $qrCodeApi.$apiUrl; ?>"><br>
+			<textarea name="ac_vcard__qrcode_embed" ><img  width='100px' height='100px' src="<?php echo $qrCodeApi.$apiUrl; ?>"></textarea>
+			</td>            
+        </tr>
+		<tr>
+
+			<th><label for="ac_vcard__qrcode_embed"><?php _e("Data QR code", 'ac_vcard'); ?></label></th>
+			<td>
+			<?php 
+				$fileName = ac_vcard__GetFileNameCore($ac_postID).'.png';	
+				if (file_exists(ac_vcard_config()["rootDir"].$fileName)) {
+					echo '<img width="180px" height="180px" src="'.ac_vcard_config()["rootUrl"].$fileName.'">';
+					echo '<br>';
+					?>
+					<textarea><img style="max-width:200px; height:auto;" src="<?php echo ac_vcard_config()["rootUrl"].$fileName; ?>"></textarea>
+					<?php
+				}else{
+					echo "no image";
+				}
+				
+			?>
 			</td>            
         </tr>
 		<tr>
@@ -68,7 +87,7 @@ function ac_vcard_cpt_display_callback( $post ) {
         </tr>
 		<tr data-name="cpt_acvcard__name">
 			<th><label for="ac_vcard__user_fname"><?php _e("First name", 'ac_vcard'); ?></label></th>
-			<td><input name="ac_vcard__user_fname" value="<?php echo $cpt_PostMeta['ac_vcard__user_fname'][0]; ?>"></td>            
+			<td><input name="ac_vcard__user_fname" id='ac_user_fname' value="<?php echo $cpt_PostMeta['ac_vcard__user_fname'][0]; ?>"></td>            
         </tr>
 		<tr data-name="cpt_acvcard__name">
 			<th><label for="ac_vcard__user_lname"><?php _e("Last name", 'ac_vcard'); ?></label></th>
@@ -83,7 +102,9 @@ function ac_vcard_cpt_display_callback( $post ) {
 			<td><input name="ac_vcard__user_cpompanyname" value="<?php echo $cpt_PostMeta['ac_vcard__user_cpompanyname'][0]; ?>"></td>            
         </tr>
 		<tr>
-			<th><label for="ac_vcard__user_fulladress"><?php _e("Full address (optional)", 'ac_vcard'); ?></label></th>
+			<th><label for="ac_vcard__user_fulladress"><?php _e("Full address (optional)", 'ac_vcard'); ?></label><br>
+			<?php _e("Format: [street, postcode, city, country]", 'ac_vcard'); ?>
+			</th>
 			<td><input name="ac_vcard__user_fulladress" value="<?php echo $cpt_PostMeta['ac_vcard__user_fulladress'][0]; ?>"></td>            
         </tr>
 		<tr>
@@ -91,7 +112,7 @@ function ac_vcard_cpt_display_callback( $post ) {
 			<td><input name="ac_vcard__user_phonenumber" value="<?php echo $cpt_PostMeta['ac_vcard__user_phonenumber'][0]; ?>"></td>            
         </tr>
 		<tr>
-			<th><label for="ac_vcard__user_mobilenumber"><?php _e("Mobile phone (optional)", 'ac_vcard'); ?></label></th>
+			<th><label for="ac_vcard__user_mobilenumber"><?php _e("Mobile phone (optional) - Preferred", 'ac_vcard'); ?></label></th>
 			<td><input name="ac_vcard__user_mobilenumber" value="<?php echo $cpt_PostMeta['ac_vcard__user_mobilenumber'][0]; ?>"></td>            
         </tr>
 		<tr>
@@ -109,18 +130,28 @@ function ac_vcard_cpt_display_callback( $post ) {
 	?>
             <script>
                 document.addEventListener("DOMContentLoaded", function() {
+					
+					var titleMainInput = document.getElementById("title");
+					titleMainInput.style.pointerEvents = "none";
+					//titleMainInput.style.opacity = "0";
+					
+					document.getElementById("ac_user_fname").focus();
+					
                     // Ustawienie pola o id "title" na disabled
                     var titleInput = document.getElementById("ac_vcard__title");
                     //titleInput.disabled = true;
                     titleInput.style.pointerEvents = "none";
 
                     function updateTitle() {
+						titleMainInput.value = '';
                         titleInput.value = ''; // Wyczyść aktualną wartość pola "title"
+
                         var divs = document.querySelectorAll('tr[data-name="cpt_acvcard__name"]');
                         divs.forEach(function(div) {
                             var input = div.querySelector('input');
                             if (input) {
                                 titleInput.value += input.value + '_';
+								titleMainInput.value += input.value + ' ';
                             }
                         });
                     }
@@ -132,6 +163,38 @@ function ac_vcard_cpt_display_callback( $post ) {
 
                     // Wywołaj funkcję raz na starcie, aby początkowa wartość pola "title" została ustawiona
                     updateTitle();
+					
+					
+					var nowTime = new Date();
+					setInterval(function () {
+						nowTime = new Date();
+						nowTime = formatDate(nowTime);
+						//document.getElementById("ac_vcard__date_update").value = nowTime;
+						
+					}, 1000);
+					
+					function formatDate(date) {
+						var d = new Date(date),
+							month = '' + (d.getMonth() + 1),
+							day = '' + d.getDate(),
+							year = d.getFullYear();
+							hour = d.getHours(), 
+							minute = d.getMinutes(), 
+							second = d.getSeconds();
+							
+							
+						if (hour < 10) 
+							hour = '0' + hour;
+						if (minute < 10) 
+							minute = '0' + minute;
+						if (second < 10) 
+							second = '0' + second;
+						if (month.length < 2) 
+							month = '0' + month;
+						if (day.length < 2) 
+							day = '0' + day;
+						return [year, month, day, hour, minute, second].join('');
+					}
                     
                 });
             </script>  
@@ -154,12 +217,14 @@ function ac_vcard_cpt_save_meta_box( $post_id ) {
 			'ac_vcard__user_mobilenumber',
 			'ac_vcard__user_emailadress',
 			'ac_vcard__user_webside',
+			'ac_vcard__date_update'
 		);
 		foreach ($fieldArray as $fieldName) {
 			update_post_meta( $ac_postID, $fieldName, $_POST[$fieldName]);
 		}
+		update_post_meta( $ac_postID, 'ac_vcard__date_update', date("YmdHis"));
+		ac_vcard__generateVCF($post_id);
 	}
-	//update_post_meta( $post_id, "add_article_main", array( "adf", "bfdf" ) )
 }
 add_action( 'save_post', 'ac_vcard_cpt_save_meta_box' );
 
@@ -177,7 +242,7 @@ function ac_vcard_cpt_checkpost($value){
 
 function ac_vcard_get_postid_by_slug( $page_slug = '', $posttype = 'ac_vcard_cpt' ) {
 	if ( $page_slug ) {
-	$postobject = get_page_by_path( $page_slug, OBJECT, $posttype );
+		$postobject = get_page_by_path( $page_slug, OBJECT, $posttype );
 		if ( $postobject ) {
 			return $postobject->ID;
 		}
